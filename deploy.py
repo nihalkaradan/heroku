afe4214246bc50992258ea83860a8960377ed139
Nihal <nihalkaradan@gmail.com>
2018-01-31 22:07:02 +0530

    cpassword removed

--- deploy.py
+++ deploy.py
@@ -12,7 +12,7 @@ class  users(db.Model):
 	id=db.Column(db.Integer,primary_key=True)
 	username=db.Column(db.String(200),unique=True,nullable=False)
 	password=db.Column(db.String(200),nullable=False)
-	cpassword=db.Column(db.String(200),nullable=False)
+	
 db.create_all()	
 
 
